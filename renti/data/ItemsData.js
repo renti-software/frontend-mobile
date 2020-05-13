279535a824f28b31290a66b649b936524f5c9522
Tom1k <tomascosta@ua.pt>
2020-05-13 15:58:21 +0100

    Added hardcoded data for listing usage

---
+++ renti/data/ItemsData.js
@@ -0,0 +1,32 @@
+var itemsList = [
+    {
+        "id" : "1",
+        "name" : "GoPro Hero5",
+        "image" : "../assets/images/data/gopro.jpg",
+        "location" : "Aveiro, Portugal",
+        "price" : "20.00"
+    },
+    {
+        "id" : "2",
+        "name" : "Muletas",
+        "image" : "../assets/images/data/muletas.jpg",
+        "location" : "Porto, Portugal",
+        "price" : "2.00"
+    },
+    {
+        "id" : "3",
+        "name" : "Boss RC-505",
+        "image" : "../assets/images/data/bossrc.jpg",
+        "location" : "Aveiro, Portugal",
+        "price" : "52.50"
+    },
+    {
+        "id" : "4",
+        "name" : "Fato Homem - M",
+        "image" : "../assets/images/data/suit.jpg",
+        "location" : "Lisboa, Portugal",
+        "price" : "18.00"
+    },
+]
+
+module.exports = itemsList;
