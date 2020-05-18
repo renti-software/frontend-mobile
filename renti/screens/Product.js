0fece8105cb0e7ea91c4073db52072449907583f
Tom1k <tomascosta@ua.pt>
2020-05-18 17:35:37 +0100

    Initial structure for centered items

--- renti/screens/Product.js
+++ renti/screens/Product.js
@@ -2,42 +2,22 @@ import * as React from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import colors from '../constants/Colors'
 import style from '../constants/Style'
-import { ScrollView } from 'react-native-gesture-handler';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { SearchBar } from 'react-native-elements';
+
 import itemsList from '../data/ItemsData';
 import { Ionicons } from '@expo/vector-icons';
 import { moderateScale, verticalScale } from 'react-native-size-matters';
 
 
-export default class Marketplace extends React.Component {
+export default class Product extends React.Component {
   constructor(props){
     super(props);
   }
 
   state = { 
-    searchValue : '',
-    //esta filtragem vem da API, enviar estes campos e devolver ja os itens conforme esta filtragem
-    filters: {
-      location: '',
-      max_price: 1000,
-      min_price: 0,
-      category: '',
-    }
   }
 
   componentDidMount(){
 
-  }
-
-  updateSearch(newSearch) {
-    this.setState({
-      searchValue: newSearch
-    })
-  }
-
-  makeSearchUpdate() {
-    alert(this.state.searchValue)
   }
 
   //GET Request
@@ -74,47 +54,12 @@ export default class Marketplace extends React.Component {
       });
   }
 
-
-  renderItems() {
-    return itemsList.map( item => {
-      //Fazer aqui um filtro pelo item.name
-      if(item.name.toLowerCase().includes(this.state.searchValue.toLowerCase())){
-        return(
-        <TouchableOpacity onPress={() => alert(`Going to page ${item.name}`)}>
-          <View style={styles.items}>
-              <Image
-                style={{height:110,padding:20,width:110,borderRadius:5,flex:1}}
-                source={{uri: item.image}}
-              >
-              </Image>
-      
-              <View style={{flexDirection:'column',flex:2}}>
-                <View style={{flexDirection:'row',justifyContent:'flex-start',alignContent:'center', marginLeft:20}}>
-                  <Text style={{fontSize:style.h3,fontWeight:'bold'}}>{item.name}</Text>
-                </View>
-      
-                <View style={{flexDirection:'row',justifyContent:'flex-start',alignContent:'center', marginLeft:20}}>
-                  <Text style={{}}>{item.location}</Text>
-                </View>
-      
-                <View style={{flexDirection:'column',justifyContent:'flex-end',alignItems:'flex-start', marginLeft:20,paddingVertical:20}}>
-                  <Text style={{color:colors.primary, fontWeight:'bold',fontSize:style.h2}}>{item.price}€ /day</Text>
-                </View>
-              
-              </View>
-      
-            </View>
-          </TouchableOpacity>
-        )
-      }
-    })
-    
-  }
-
   render() {
     return (
-      <View>
-
+      <View style={styles.container}>
+        <View style={styles.column_container}>
+            <Text>Ola</Text>
+        </View>
       </View>
     );
   }
@@ -123,9 +68,16 @@ export default class Marketplace extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.light_gray,
+    alignItems: 'center',
     paddingHorizontal:10,
-    marginTop: 30 //TODO change to android top window
+    marginTop: 30 //TODO change to android top windo
+  },
+  column_container: {
+    flex: 1,
+    flexDirection:'row',
+    alignItems: 'center',
+    paddingHorizontal:10,
+    marginTop: 30 //TODO change to android top windo
   },
   items: {
     flex:1,
