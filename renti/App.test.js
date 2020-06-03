b3f364f855c1f1748016a73e9563c721ba4748cc
Tom1k <tomascosta@ua.pt>
2020-06-03 20:02:08 +0100

    Cannot perform unit testing on mobile, due to package config

--- renti/App.test.js
+++ renti/App.test.js
@@ -4,9 +4,9 @@ import renderer from 'react-test-renderer';
 import Product from './screens/Product'
 import App from './App';
 
-describe('<Product />', () => {
+describe('<App />', () => {
   it('has 1 child', () => {
-    const tree = renderer.create(<Product />).toJSON();
+    const tree = renderer.create(<App />).toJSON();
     expect(tree.children.length).toBe(1);
   });
 });
